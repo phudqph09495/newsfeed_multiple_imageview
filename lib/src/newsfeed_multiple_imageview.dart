33c334a1e67d3e2a76b7edab49793832d35f1017
phudqph0311 <71993940+phudqph09495@users.noreply.github.com>
2026-09-11 18:25:58 +0700

    Update newsfeed_multiple_imageview.dart

--- lib/src/newsfeed_multiple_imageview.dart
+++ lib/src/newsfeed_multiple_imageview.dart
@@ -48,29 +48,58 @@ class NewsfeedMultipleImageView extends StatelessWidget {
     );
   }
 }
-
-class ImageViewer extends StatelessWidget {
+class ImageViewer extends StatefulWidget {
   final List<String> imageUrls;
-  const ImageViewer({Key? key,
+
+  const ImageViewer({
+    Key? key,
     required this.imageUrls,
   }) : super(key: key);
 
   @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  late final PageController _pageController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController(
+      initialPage: _selectedIndex,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _selectImage(int index) {
+    if (_selectedIndex == index) return;
+
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(
-      bottom: false,
-      child: Container(
-        // width: MediaQuery.of(context).size.width,
-        // height: MediaQuery.of(context).size.height,
-        color: Colors.black,
-        child: SafeArea(
-          top: false,
-          left: false,
-          right: false,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
+    if (widget.imageUrls.isEmpty) {
+      return Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Stack(
             children: [
               IconButton(
                 onPressed: () => Navigator.pop(context),
@@ -80,31 +109,191 @@ class ImageViewer extends StatelessWidget {
                   size: 30,
                 ),
               ),
-              Expanded(
-                child: ImageSlideshow(
-                  initialPage: 0,
-                  indicatorColor: Colors.red,
-                  indicatorBackgroundColor: Colors.grey,
-                  isLoop: imageUrls.length > 1,
-                  children: imageUrls
-                      .map(
-                        (e) => ClipRect(
-                      child: SmartImage(
-                        e,
-                        fit: BoxFit.contain,
-                        isPost: true,
-                      ),
-                    ),
-                  )
-                      .toList(),
+              const Center(
+                child: Text(
+                  'Không có hình ảnh',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ],
           ),
         ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Header
+            SizedBox(
+              height: 56,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+                  const Spacer(),
+
+                  // Hiển thị vị trí ảnh hiện tại
+                  Text(
+                    '${_selectedIndex + 1}/${widget.imageUrls.length}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+
+                  const SizedBox(width: 20),
+                ],
+              ),
+            ),
+
+            // Ảnh lớn
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: widget.imageUrls.length,
+                onPageChanged: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 4,
+                    ),
+                    child: SmartImage(
+                      widget.imageUrls[index],
+                      fit: BoxFit.contain,
+                      isPost: true,
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            // Danh sách ảnh preview
+            if (widget.imageUrls.length > 1)
+              Container(
+                height: 92,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 10,
+                ),
+                color: Colors.black,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: widget.imageUrls.length,
+                  separatorBuilder: (_, __) =>
+                      const SizedBox(width: 8),
+                  itemBuilder: (context, index) {
+                    final bool isSelected =
+                        index == _selectedIndex;
+
+                    return GestureDetector(
+                      onTap: () => _selectImage(index),
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 200),
+                        width: 68,
+                        height: 68,
+                        padding: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            color: isSelected
+                                ? Colors.white
+                                : Colors.transparent,
+                            width: 2,
+                          ),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
+                          child: Opacity(
+                            opacity: isSelected ? 1 : 0.6,
+                            child: SmartImage(
+                              widget.imageUrls[index],
+                              fit: BoxFit.cover,
+                              isPost: true,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
+// class ImageViewer extends StatelessWidget {
+//   final List<String> imageUrls;
+//   const ImageViewer({Key? key,
+//     required this.imageUrls,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(body: SafeArea(
+//       bottom: false,
+//       child: Container(
+//         // width: MediaQuery.of(context).size.width,
+//         // height: MediaQuery.of(context).size.height,
+//         color: Colors.black,
+//         child: SafeArea(
+//           top: false,
+//           left: false,
+//           right: false,
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             mainAxisSize: MainAxisSize.max,
+//             children: [
+//               IconButton(
+//                 onPressed: () => Navigator.pop(context),
+//                 icon: const Icon(
+//                   Icons.close,
+//                   color: Colors.white,
+//                   size: 30,
+//                 ),
+//               ),
+//               Expanded(
+//                 child: ImageSlideshow(
+//                   initialPage: 0,
+//                   indicatorColor: Colors.red,
+//                   indicatorBackgroundColor: Colors.grey,
+//                   isLoop: imageUrls.length > 1,
+//                   children: imageUrls
+//                       .map(
+//                         (e) => ClipRect(
+//                       child: SmartImage(
+//                         e,
+//                         fit: BoxFit.contain,
+//                         isPost: true,
+//                       ),
+//                     ),
+//                   )
+//                       .toList(),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     ));
+//   }
+// }
 
 
